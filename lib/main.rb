f572040cc6765b92c8c03f3f3e4e23bc3436d118
Edin <edyfer2@live.com.au>
2024-09-13 10:42:34 +0200

    Edit comment

--- lib/main.rb
+++ lib/main.rb
@@ -1,2 +1,2 @@
 #Create variable called dictionary to store all the contents from the file called
-#google-10000-english-no-swears.txt and return as an array into memory
+#google-10000-english-no-swears.txt line by line and return as an array into memory
