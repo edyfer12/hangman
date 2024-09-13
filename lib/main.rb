fac721ae1689cbf04161bf4f50bb0a41be30e61c
Edin <edyfer2@live.com.au>
2024-09-13 11:01:13 +0200

    Load all contents from external file

--- lib/main.rb
+++ lib/main.rb
@@ -2,3 +2,5 @@
 #google-10000-english-no-swears.txt line by line and return as an array into memory.
 #This will be used by the computer to pick out the random word for the guesser to select
 #the letter for the mystery word.
+
+dictionary = File.readlines('google-10000-english-no-swears.txt')
