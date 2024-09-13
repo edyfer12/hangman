ae5d0d47b7561f0cbdaba42e69bf4fa0ab1618b2
Edin <edyfer2@live.com.au>
2024-09-13 15:13:06 +0200

    Fix the bug that prints out the words

--- lib/main.rb
+++ lib/main.rb
@@ -9,10 +9,9 @@ dictionary = File.readlines('../google-10000-english-no-swears.txt')
 #chosen word from the dictionary array that has between 5 and 12 characters which is one
 #of the rules created for the host(or computer) of the game.
 
-restricted_words = dictionary.map do |word|
+restricted_words = dictionary.filter do |word|
   if word.strip.length >= 5 && word.strip.length <= 12
     word
-  else
-    word.strip
   end
 end
+puts restricted_words
