25410ed46ea464fd884e623c8fa47f233c909369
Edin <edyfer2@live.com.au>
2024-09-13 10:45:24 +0200

    Edit comment

--- lib/main.rb
+++ lib/main.rb
@@ -1,2 +1,4 @@
 #Create variable called dictionary to store all the contents from the file called
-#google-10000-english-no-swears.txt line by line and return as an array into memory
+#google-10000-english-no-swears.txt line by line and return as an array into memory.
+#This will be used by the computer to pick out the random word for the guesser to select
+#the letter for the mystery word.
