f9d9ec0fb594c60c3d9c9a79fc2137a838ae5335
Edin <edyfer2@live.com.au>
2024-09-15 13:56:03 +0200

    Add empty line

--- lib/main.rb
+++ lib/main.rb
@@ -139,6 +139,7 @@ class Game
       #Print the updated guesser_word array
       puts @guesser_word.join(' ')
       #Display an empty line to enable the program to appear presentable
+      puts
       #Print out incorrect_guesses_left
       puts "Incorrect guesses left: #{@incorrect_guesses_left}"
       #Print out incorrect_inputs array if the array is not empty
